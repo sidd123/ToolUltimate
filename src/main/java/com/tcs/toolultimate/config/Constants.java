a80d97a98f7aa36fcd687ebd590906b42e75dca7
457590 <457590@01HW463696.India.TCS.com>
2016-04-06 15:34:48 +0530

    Constatns value changed

--- src/main/java/com/tcs/toolultimate/config/Constants.java
+++ src/main/java/com/tcs/toolultimate/config/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	public static final String COLUMN_NAME_ACCOUNT_NAME = "account_name";
 	public static final String COLUMN_NAME_ACCOUNT_ID = "account_id";
 	public static final String STATUS = "status";
-	public static final String SUCCESS = "succsess";
+	public static final String SUCCESS = "success";
 	public static final String FAIL = "fail";
 	public static final String DETAILS = "details";
 }
