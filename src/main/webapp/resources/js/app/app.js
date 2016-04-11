020c9a19d163abe9070dee9c7a5a33f7016ea28a
912343 <912343@01HW757635.India.TCS.com>
2016-04-11 16:30:31 +0530

    app.js

--- src/main/webapp/resources/js/app/app.js
+++ src/main/webapp/resources/js/app/app.js
@@ -39,27 +39,28 @@ app.config([ '$routeProvider', function($routeProvider) {
 		templateUrl : 'resources/views/viewSubProject.html',
 		controller : 'accountCtrl'
 	});
-} ]).run(
-		[
-				'$rootScope',
-				'$location',
-				'Data',
+}]).run(['$rootScope', '$location', 'Data',
 				function($rootScope, $location, Data) {
-					$rootScope.$on("$routeChangeStart", function(event, next,
-							current) {
-						$rootScope.authenticated = false;
-						/*
-						 * Data.get('login').then(function (results) { if
-						 * (results.name) { $rootScope.authenticated = true;
-						 * $rootScope.uid = results.uid; $rootScope.name =
-						 * results.name; $rootScope.email = results.email; }
-						 * else { var nextUrl = next.$$route.originalPath; if
-						 * (nextUrl == '/signup' || nextUrl == '/login') {
-						 *  } else { $location.path("/login"); } } });
-						 */
-					});
-				} ]).run([ '$rootScope', function($rootScope) {
-	$rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
-		$rootScope.title = current.$$route.title;
-	});
-} ]);
+					$rootScope.$on("$routeChangeStart", 
+							function(event, next, current) {
+								Data.get('sessionexists').then(function (results) {
+					                if (results.uid) {
+					                    $rootScope.uid = results.uid;
+					                } else {
+					                    var nextUrl = next.$$route.originalPath;
+					                    if (nextUrl == '/signup' || nextUrl == '/login') {
+		
+					                    } else {
+					                        $location.path("/login");
+					                    }
+					                }
+					            });
+								
+							});
+}]).run(['$rootScope', 
+  		function($rootScope) {
+				$rootScope.$on('$routeChangeSuccess', 
+						function(event, current, previous) {
+							$rootScope.title = current.$$route.title;
+						});
+}]);
