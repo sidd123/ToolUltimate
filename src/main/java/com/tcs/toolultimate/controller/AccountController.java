d0c84e2382f04f975644cb696049bf527e168fbc
457590 <457590@01HW463696.India.TCS.com>
2016-04-13 15:17:59 +0530

    Serviced Changed

--- src/main/java/com/tcs/toolultimate/controller/AccountController.java
+++ src/main/java/com/tcs/toolultimate/controller/AccountController.java
@@ -1,9 +1,15 @@
 package com.tcs.toolultimate.controller;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,36 +17,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tcs.toolultimate.config.Constants;
+import com.tcs.toolultimate.config.SearchHelper;
 import com.tcs.toolultimate.service.AccountService;
 import com.tcs.toolultimate.vo.Account;
+import com.tcs.toolultimate.vo.Search;
 import com.tcs.toolultimate.vo.SubProjects;
 
 @Controller
 public class AccountController {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	AccountService accountService;
 	
+	@Autowired
+	SearchHelper helper;
 	
 	@RequestMapping(value="/viewAccount")
-	public @ResponseBody Map<String, Object> viewAccounts(){
+	public @ResponseBody Map<String, Object> viewAccounts(@RequestBody Search searchAttribute){
+		Map<String, Object> finalResults = null;
 		List<Account> fetchAllAcounts = accountService.fetchAllAcounts();
-		Map<String, Object> accountDetails = new HashMap<String, Object>();
-		accountDetails.put(Constants.STATUS, fetchAllAcounts.size() > 0 ? Constants.SUCCESS : Constants.FAIL);
-		accountDetails.put(Constants.DETAILS, fetchAllAcounts);
-		accountDetails.put(Constants.TOTAL_RECORDS, fetchAllAcounts.size());
-		return accountDetails;
+		try {
+			finalResults = helper.getFinalResults(fetchAllAcounts, searchAttribute);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			logger.error("Error occured in method viewAccounts: " + e);
+		}
+		return finalResults;
 	}
 	
 	@RequestMapping(value="/saveAccount")
 	public @ResponseBody Map<String, Object> saveAccount(@RequestBody Account account){
+		Map<String, Object> finalResults = null;
 		accountService.saveOrUpdateAccount(account);
 		List<Account> fetchAllAcounts = accountService.fetchAllAcounts();
-		Map<String, Object> accountDetails = new HashMap<String, Object>();
-		accountDetails.put(Constants.STATUS, fetchAllAcounts.size() > 0 ? Constants.SUCCESS : Constants.FAIL);
-		accountDetails.put(Constants.DETAILS, fetchAllAcounts);
-		accountDetails.put(Constants.TOTAL_RECORDS, fetchAllAcounts.size());
-		return accountDetails;
+		try {
+			finalResults = helper.getFinalResults(fetchAllAcounts, new Search());
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			logger.error("Error occured in method viewAccounts: " + e);
+		}
+		return finalResults;
 	}
 	
 	@RequestMapping(value="/editAccount")
