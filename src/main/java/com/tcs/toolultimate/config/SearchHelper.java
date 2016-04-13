b0573e6319ca6883b724539018c065f1920bb8e2
457590 <457590@01HW463696.India.TCS.com>
2016-04-13 15:41:12 +0530

    Removed unused import

--- src/main/java/com/tcs/toolultimate/config/SearchHelper.java
+++ src/main/java/com/tcs/toolultimate/config/SearchHelper.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 
 import com.tcs.toolultimate.vo.Search;
 
